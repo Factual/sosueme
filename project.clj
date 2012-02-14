e37f848939cdc10f9ce4e9ce295fadbe19f40017
Aaron Crow <dirtyvagabond@yahoo.com>
2012-02-13 18:39:39 -0800

    Added :url to project

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "A collection of Clojure utilities"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [fs "1.0.0"]]
-  :dev-resources-path "test/resource")
+  :dev-resources-path "test/resource"
+  :url "https://github.com/Factual/sosueme")
                  
