2b534c1e85ad3842691f90c33b33b0063af5c3b2
Spencer Tipping <spencer@spencertipping.com>
2012-12-10 14:52:51 -0800

    Updates for defnk

--- src/sosueme/fn.clj
+++ src/sosueme/fn.clj
@@ -3,6 +3,8 @@
   (:use [clojure.string :only [join]]
         [clojure.set :only [difference]]))
 
+(def defnk-default ::defnk-default)
+
 (defmacro defnk
   "The macro that jackknife.def/defnk always should have been. The only real difference is that we
   generate a proper {:keys :or} map instead of doing varargs processing. This improves the
@@ -16,6 +18,8 @@
   3. Keyword arguments can contain an optional docstring after the default value. If you do this,
      that docstring is appended to the function's docstring in a standard way. Positional arguments
      can be documented similarly, but the docstring follows the name since there is no value.
+  4. You can use the default value for a keyword argument by passing the value defnk-default. This
+     allows you to chain defnk invocations and write the default values only once.
 
   For example:
 
@@ -55,13 +59,20 @@
                                          (cons (f k default) (map-keywords f (cons doc? rest))))))
 
         recognized-keywords        (set (map-keywords (fn [k & rest] k) keywords))
-        default-map                (into {} (map-keywords (fn [k v & [doc]] [(symbol (name k)) v])
-                                                          keywords))
+        default-map                (into {} (map-keywords (fn [k v & [doc]] [k v]) keywords))
 
         keyword-formals            (map-keywords (fn [k & rest] (symbol (name k))) keywords)
         keyword-docs               (into {} (map-keywords (fn [k v & [doc]] (when doc [k doc]))
                                                           keywords))
         keyword-defaults           (apply concat (map-keywords (fn [k v & rest] [k v]) keywords))
+        scoped-defaults            (into {} (map-keywords (fn [k v & [doc]] [(symbol (name k))
+                                                                             defnk-default])
+                                                          keywords))
+        default-map-gensym         (gensym "default-map")
+        default-assignments        (mapcat #(vector % `(if (= ~% ~defnk-default)
+                                                         ~(get default-map (keyword %))
+                                                         ~%))
+                                           keyword-formals)
 
         positional-width           (reduce max 0 (map (comp count str) positional-args))
         keyword-width              (reduce max 0 (map-keywords (fn [k v & rest]
@@ -89,11 +100,13 @@
                                                                positional-docstring
                                                                keyword-docstring]))]
 
-    `(do (defn ~fn-name ~final-docstring [~@positional-args
-                                          & {:keys ~keyword-formals :or ~default-map :as a#}]
+    `(do (defn ~fn-name ~final-docstring [~@positional-args &
+                                          {:keys ~keyword-formals :or ~scoped-defaults :as a#}]
            (when-let [strays# (seq (difference (set (keys a#)) ~recognized-keywords))]
              (throw (Exception. (str "invalid keyword args for " '~fn-name ": " strays#))))
-           ~@body)
+           (let [~default-map-gensym ~default-map
+                 ~@default-assignments]
+             ~@body))
          (alter-meta! (var ~fn-name) assoc :arglists '(~(vec (concat positional-args
                                                                      (seq keyword-defaults)))))
          (var ~fn-name))))
